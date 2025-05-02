a40960bef1206650326be713fa5f31f94f252697
smallfoot47 <smallfoot@osiris.cyber.nyu.edu>
2025-05-02 15:17:15 -0400

    Wrote _gen_primes function

--- detect.py
+++ detect.py
@@ -9,7 +9,34 @@ class Detective:
         assert(limit > 0)
 
         self.__lvl = self._gen_primes(limit)
+        # print(self.__primes)
+        print(self.__lvl)
+
         pass
-    
-    def gen_primes(self, limit:int):
+
+    def _gen_primes(self, limit:int) -> int:
+        """
+        upto the limit, collects all primes. Returns number of primes, pi(x)
+        Uses Sieve of Eratosthenes
+        """
+        if limit < 2:
+            self.__primes = []
+            return 0
+        
+        primes = [ 2 ]
+
+        for number in range(3, limit + 1):
+            isPrime = True
+            for prime in primes:
+                if number % prime == 0:
+                    isPrime = False
+            
+            if isPrime:
+                primes.append(number)
+        
+        self.__primes = primes
+
+        return len(primes)
+
+    def detect(self):
         pass
