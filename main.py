977702a8b6e28c6eee77cb97a8c77ff2a0eabe07
smallfoot47 <smallfoot@osiris.cyber.nyu.edu>
2025-05-02 14:10:26 -0400

    Mentioned inputs and outputs in doc comments

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from detect import *
 from localize import *
 
 '''
+INPUT: hash, hash
+OUTPUT: corrupt hash indicies
+
 We send the hash of OG msg and received msg and compare the hashes
 any hash that is not EXACTLY the same is corrupt and gets sent to localization
 '''
@@ -9,6 +12,9 @@ def corruptHashDetect():
     return 0
 
 '''
+INPUT: corrupt hash indicies
+OUTPUT: lower and upper bounds
+
 This is to narrow down the exact start and end index of the corruption
 We use binary search to first find the corruption in general
 options to narrow it down further
@@ -16,7 +22,6 @@ options to narrow it down further
 so you could instead of checking in halves based on the lenght of the hash
 we now check based on halves of the corruption length
 - check each adjacent bit/index and compare w og hash and keep going until we get to the start or end
-- ???
 '''
 def localization():
     return 0
