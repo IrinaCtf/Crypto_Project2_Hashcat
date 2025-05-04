bf07034e9ff9d97ddf824c598982db12913aca22
NatSR4 <sneha.ranjit4@gmail.com>
2025-05-04 19:46:30 -0400

    Half way debugged localize func only

--- localize.py
+++ localize.py
@@ -1,6 +1,6 @@
 
 #when calling send M hash, M' hash
-def binary_search(ogHash, cHash):
+def binary_search(ogHash, cHash, mid):
     high = len(ogHash)
     low = 0
 
@@ -12,29 +12,29 @@ def binary_search(ogHash, cHash):
         # If element is present at the middle itself
         if ogHash[mid] != cHash[mid]:
             print(ogHash +" "+ cHash)
-            return (mid, cHash)
+            return (ogHash, cHash, mid)
 
         # If element is equal at mid, then it can only
         # be present in left subarray or the latter half
         elif ogHash[mid] == cHash[mid]:
             print(ogHash +" "+ cHash)
-            return binary_search(ogHash[mid+1:], cHash[mid+1:])
+            return binary_search(ogHash[mid+1:], cHash[mid+1:], mid)
 
     else:
         # Everything is equal
         print("equal")
-        return (-1, ogHash)
+        return (ogHash,cHash,0)
 
 
 def localize(ogHash, cHash, start_index, diff):
 
     corrupt_len = len(cHash)
-    checked = 0
+    checked = 1
     counter = 0
     k = start_index
     begin_corrupt_index = -1
     end_corrupt_index = -1
-
+    print("start w "+ str(k))
     '''
     case 0: only one index was corrupted so checked needs to start at 0
     this can mean that we can stop searching too hence the break
@@ -46,30 +46,48 @@ def localize(ogHash, cHash, start_index, diff):
     so we don't need to keep checking that end
     '''
     while(counter < corrupt_len):
-
-        #check end
-        if(end_corrupt_index == -1 and cHash[k+checked] == ogHash[k+checked]):
-            end_corrupt_index = k + checked - 1
-            counter += 1
-        #check beginning
-        if(begin_corrupt_index == -1 and cHash[k-checked] == ogHash[k-checked]):
-            begin_corrupt_index = k - checked + 1
-            counter += 1
-
+        print(counter)
         #check that we haven't reached the end
-        if(k + checked == corrupt_len):
+        if(k + checked >= corrupt_len):
             end_corrupt_index = corrupt_len
         #check that we haven't reached the start
-        if(k - checked == 0):
+        if(k - checked < 0):
             begin_corrupt_index = 0
         
         #check that we found the whole corruption
         if(begin_corrupt_index != -1 and end_corrupt_index != -1):
             break
-        
+
+        #check end
+        if(end_corrupt_index == -1 and cHash[k+checked] == ogHash[k+checked]):
+            end_corrupt_index = k + checked - 1
+            #checked += 1
+            print("end counter "+ str(counter))
+
+        #check beginning
+        if(begin_corrupt_index == -1 and cHash[k-checked] == ogHash[k-checked]):
+            begin_corrupt_index = k - checked + 1
+            #checked += 1
+            print("begin counter "+ str(counter))
+        checked +=1
+        counter +=1
+    return (begin_corrupt_index, end_corrupt_index, cHash[begin_corrupt_index:end_corrupt_index])  
 
 
 
 
+print("CASE 1")
+ogHash, cHash, start_index = binary_search("HelloHelloHello","HelloHelloHe110",-1)
+print(localize(ogHash, cHash, start_index, 3))
 
-binary_search("HelloHelloHello","HelloHelloHello")
+print("CASE 2")
+ogHash, cHash, start_index = binary_search("HelloHelloHello","HelloHelloHe1lo", -1)
+print(localize(ogHash, cHash, start_index, 3))
+
+print("CASE 3")
+ogHash, cHash, start_index = binary_search("HelloHelloHello","HelloBYeyeHe1lo", -1)
+print(localize(ogHash, cHash, start_index, 3))
+
+print("CASE 4")
+ogHash, cHash, start_index = binary_search("HelloHelloHello","BybebHelloHello", -1)
+print(localize(ogHash, cHash, start_index, 3))
